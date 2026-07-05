1cb52023511007d0224a030e38927eaecd7f4530
Computery <77170025+C0mputery@users.noreply.github.com>
2026-07-04 23:03:53 -0400

    A totally reasonable upgrade for full test coverage across full ulong offsets

--- ComputerysBitStream.Tests/BitOffsetRange.cs
+++ ComputerysBitStream.Tests/BitOffsetRange.cs
@@ -1,9 +1,9 @@
 namespace ComputerysBitStream.Tests;
 
 public class BitOffsetRange : TheoryData<int> {
-    public BitOffsetRange() { AddRange(Enumerable.Range(0, 16)); }
+    public BitOffsetRange() { AddRange(Enumerable.Range(0, 128)); }
 }
 
-public class ZeroBitOffsetRange : TheoryData<int> {
+public sealed class ZeroBitOffsetRange : TheoryData<int> {
     public ZeroBitOffsetRange() { Add(0); }
 }
